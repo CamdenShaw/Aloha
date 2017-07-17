fcb4d56fcc262104b8bbff63d3b8ee7c78f87266
Camden_Shaw <camden.shaw@gmail.com>
2017-07-17 11:35:08 -0700

    -a

--- js/perkyScript.js
+++ js/perkyScript.js
@@ -1,7 +1,23 @@
 console.log('Hello!  Do I exist?');
 
+var windowW = window.innerWidth;
+
 $('.main-carousel').flickity({
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  cellSelector: '.carousel-cell',
+  prevNextButtons: false,
+  autoPlay: true,
+  groupCells: 1
 });
+
+if (windowW === 600) {
+  $('.main-carousel').flickity({
+    groupCells: 2,
+    autoPlay: true,
+    
+  })
+}
+
+// data-flickity='{ "cellSelector": ".carousel-cell", "prevNextButton": false, "groupCells": 80%, "autoPlay": true }'
