2bbb192cdc3583813c0e7b6695da59f978246d46
Camden_Shaw <camden.shaw@gmail.com>
2017-07-17 17:59:41 -0700

    -a

--- js/perkyScript.js
+++ js/perkyScript.js
@@ -38,30 +38,19 @@ else if (windowW===1240) {
       }, 800, function(){
         window.location.hash = hash;
       });
-    }
+    } 
   });
 });
-// $(document).ready(function(){
-//   // Add smooth scrolling to all links
-//   $("a").on('click', function(event) {
 
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
-
-//       // Store hash
-//       var hash = this.hash;
-
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 800, function(){
-   
-//         // Add hash (#) to URL when done scrolling (default click behavior)
-//         window.location.hash = hash;
-//       });
-//     } // End if
-//   });
-// });
+$(document).ready(function(){
+  var checkEmail = $('.email').value;
+  $(this).on('click', 'button', function(){
+    $('.email').setAttribute('value:','')
+    var email = $('.email').text.checkValidity();
+    if (email === false){
+      window.alert('not a valid email');
+    }
+    else if (email === true)
+      window.alert('Thank you for Subscribing');
+  });
+});
